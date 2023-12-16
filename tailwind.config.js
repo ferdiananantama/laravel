f409f108a65df358943dccdf809b97691555fa5a
ferdiananantama <ferdiananantama12@gmail.com>
2023-12-16 22:13:23 +0700

    output

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./resources/**/*.vue",
     "./node_modules/flowbite/**/*.js"
   ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
   theme: {
     extend: {
       fontFamily : {
